f23fb7a9473c723801ea97bac082af32b902f27c
chankein <chankeinjp@yahoo.co.jp>
2023-10-17 08:22:59 +0900

    update

--- waerableDeviceDemo Watch App/MotionSensor.swift
+++ waerableDeviceDemo Watch App/MotionSensor.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import CoreMotion
 import HealthKit
 import Foundation
+import Combine
 
 class MotionSensor: NSObject, ObservableObject {
     let motionManager = CMMotionManager()
@@ -16,6 +17,8 @@ class MotionSensor: NSObject, ObservableObject {
     
     var startTime: Date = Date()
     var endTime: Date = Date()
+    
+    @Published var motionDataPoints: [MotionData] = []
     
     @Published var stepCount = "0"
     @Published var calories = "0"
@@ -34,6 +37,7 @@ class MotionSensor: NSObject, ObservableObject {
     @StateObject var healthManager = HealthManager()
     
     private var timer: Timer?
+    private var dataSaveTimer: Timer?
     
     func start() {
         if motionManager.isDeviceMotionAvailable {
@@ -50,6 +54,8 @@ class MotionSensor: NSObject, ObservableObject {
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
                     self?.elapsedTime += 1
                 }
+        dataSaveTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, 
+                                             selector: #selector(saveMotionDataPoint), userInfo: nil, repeats: true)
     }
     
     func stop() {
@@ -78,6 +84,13 @@ class MotionSensor: NSObject, ObservableObject {
                 print("Updated heartRate: \(self?.heartRate ?? "nil")")
             }
         }
+        
+        // Invalidate the timer
+        dataSaveTimer?.invalidate()
+        dataSaveTimer = nil
+        
+        // Save all collected data points
+        saveAllMotionData()
     }
     
     private func updateMotionData(deviceMotion:CMDeviceMotion) {
@@ -90,9 +103,93 @@ class MotionSensor: NSObject, ObservableObject {
     }
     
     var elapsedTimeString: String {
-            let minutes = elapsedTime / 60
-            let seconds = elapsedTime % 60
-            return String(format: "%02d:%02d", minutes, seconds)
-        }
+        let minutes = Int(elapsedTime) / 60
+        let seconds = Int(elapsedTime) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+
+    @objc private func saveMotionDataPoint() {
+        let newMotionData = MotionData(
+            timestamp: Date(),
+            acceX: acceX,
+            acceY: acceY,
+            acceZ: acceZ,
+            rotX: rotX,
+            rotY: rotY,
+            rotZ: rotZ,
+            stepCount: stepCount,
+            calories: calories,
+            heartRate: heartRate
+        )
+        motionDataPoints.append(newMotionData)
+    }
     
+    private func saveAllMotionData() {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601  // Use ISO 8601 date encoding
+        
+        do {
+            let data = try encoder.encode(motionDataPoints)
+            // Now `data` contains the JSON data. You can write it to a file:
+            let documentsDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let fileURL = documentsDirectory.appendingPathComponent("motionData.json")
+            try data.write(to: fileURL)
+        } catch {
+            print("Error saving motion data: \(error)")
+        }
+    }
+    
+    func uploadDataToAPI(completion: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601  // Use ISO 8601 date encoding
+        
+        do {
+            let data = try encoder.encode(motionDataPoints)
+            let apiUrl = URL(string: "https://ryno0hub6k.execute-api.ap-northeast-1.amazonaws.com/develop/uploadMotionData")!
+            
+            var request = URLRequest(url: apiUrl)
+            request.httpMethod = "POST"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = data
+            
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                guard error == nil else {
+                    print("Error uploading data: \(error!.localizedDescription)")
+                    completion(false, error) // Call the completion with the error
+                    return
+                }
+                
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                    let apiError = NSError(domain: "", code: httpResponse.statusCode, userInfo: [NSLocalizedDescriptionKey : "API response status: \(httpResponse.statusCode)"])
+                    print("API response status: \(httpResponse.statusCode)")
+                    completion(false, apiError) // Call the completion with the API error
+                    return
+                }
+                
+                print("Data uploaded successfully!")
+                completion(true, nil) // Call the completion with success
+            }
+            
+            task.resume()
+            
+        } catch {
+            print("Error encoding motion data: \(error)")
+            completion(false, error) // <-- 添加这行
+        }
+    }
+
+
+}
+
+struct MotionData: Codable {
+    var timestamp: Date
+    var acceX: String
+    var acceY: String
+    var acceZ: String
+    var rotX: String
+    var rotY: String
+    var rotZ: String
+    var stepCount: String
+    var calories: String
+    var heartRate: String
 }
--- waerableDeviceDemo Watch App/MotionSensorScreen.swift
+++ waerableDeviceDemo Watch App/MotionSensorScreen.swift
@@ -11,158 +11,249 @@ import Foundation
 
 struct MotionSensorScreen: View {
     @ObservedObject var sensor = MotionSensor()
-    
-    var body: some View {
-        
-            VStack(spacing: 0){
-                HStack {
-                    Image(systemName: "figure.walk")
-                    Text("\(sensor.stepCount)")
-                    Spacer()
-                }
-                .offset(x: 0,y: -25)
-                Button(action: {
-                    sensor.isStarted ? sensor.stop() : sensor.start()
-                }) {
-                    ZStack() {
-                        StartBottunBackGround()
-                        Text(sensor.isStarted ? "終了" : "開始")
-                            .font(Font.custom("Manrope", size: 24).weight(.bold))
-                            .foregroundColor(Color(red: 0.97, green: 0.97, blue: 0.97))
-                    }
-                    
-                    //Text(sensor.isStarted ? "記録停止" : "記録開始")
-                    
-                }
-                .offset(x: 0,y: -10)
-                .frame(width: 100, height: 100)
-                .buttonStyle(PlainButtonStyle())
-                .onReceive(sensor.$stepCount, perform: { newValue in
-                    print("View received stepCount: \(newValue)")
-                })
-                
-                HStack{
-                    Image(systemName: "gearshape.fill")
-                        .font(.system(size: 15))
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Circle().frame(width: 24).foregroundColor(Color("DarkGray")))
-                    
-                    Text("\(sensor.elapsedTimeString)")
-                    
-                    Image(systemName: "square.and.arrow.up.fill").font(.system(size: 13))
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Circle().frame(width: 24).foregroundColor(Color("DarkGray")))
-                }
-                HStack {
-                    Image(systemName: "heart")
-                    Text("\(sensor.heartRate) BPM")
-                    Spacer()
-                    Image(systemName: "flame")
-                    Text("\(sensor.calories) kcal")
-                }
-                .foregroundColor(.accentColor)
-                //.offset(x: 0,y: 15)
-            }
-            //.sheet(isPresented: sensor.isStarted) {
-            //}
+    @State private var showTimePicker = false
+    @State private var startDate = Date()
+    @State private var endDate = Date().addingTimeInterval(60*60)
+    @State private var showAlert = false
+    @State private var alertMessage = ""
 
-          
+    var body: some View {
+        VStack(spacing: 0){
+            if !sensor.isStarted {
+                steps
+            } else {
+                accAndRot
+            }
+            
+            startAndStop.animation(.easeInOut)
+                
+            if !sensor.isStarted {
+                settingAndUpload
+            } else{
+                stepsAndTiming
+            }
+            GradientDivider()
+            heartRateAndCalories
+            GradientDivider()
+        }.animation(.easeInOut, value: sensor.isStarted)
     }
 }
 
-struct TimingView: View {
-    @ObservedObject var sensor = MotionSensor()
-  var body: some View {
-      VStack {
-          HStack{
-              Spacer()
-              VStack {
-              Text("加速度")
-                .font(.system(size: 14))
-                .foregroundColor(Color(.white))
-              Text("x: \(sensor.acceX) \ny: \(sensor.acceY) \nz: \(sensor.acceZ)")
-                .font(.system(size: 12))
-                .foregroundColor(Color(red: 0.79, green: 0.79, blue: 0.79))
-                .multilineTextAlignment(.leading)
-          }.frame(width: 70, height: 70)
-                  .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                      .inset(by: 1)
-                      .stroke(Color(red: 1, green: 0.56, blue: 0.24), lineWidth: 1)
-                  )
-              Spacer()
-              VStack {
-                  Text("回転速度")
+extension MotionSensorScreen {
+    private var steps: some View {
+        HStack {
+            Image(systemName: "figure.walk")
+            Text("\(sensor.stepCount)")
+            Spacer()
+        }
+        .offset(x: 10,y: -27)
+    }
+    
+    private var accAndRot: some View {
+        HStack{
+            Spacer()
+            VStack {
+                Text("加速度")
+                  .font(.system(size: 14))
+                  .foregroundColor(Color(.white))
+                Text("x: \(sensor.acceX) \ny: \(sensor.acceY) \nz: \(sensor.acceZ)")
+                  .font(.system(size: 12))
+                  .foregroundColor(Color(red: 0.79, green: 0.79, blue: 0.79))
+                  .multilineTextAlignment(.leading)
+            }
+            .frame(width: 70, height: 70)
+            .overlay(
+              RoundedRectangle(cornerRadius: 10)
+                .inset(by: 1)
+                .stroke(Color.accentColor, lineWidth: 1)
+            )
+            Spacer()
+            VStack {
+                Text("回転速度")
                     .font(.system(size: 12))
                     .foregroundColor(Color(.white))
-                  Text("x: \(sensor.rotX) \ny: \(sensor.rotY) \nz: \(sensor.rotZ)")
-                    .font(Font.custom("Manrope", size: 12))
+                Text("x: \(sensor.rotX) \ny: \(sensor.rotY) \nz: \(sensor.rotZ)")
+                    .font(.system(size: 12))
                     .foregroundColor(Color(red: 0.79, green: 0.79, blue: 0.79))
                     .multilineTextAlignment(.leading)
-              }
-              .frame(width: 70, height: 70)
-              .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                  .inset(by: 1)
-                  .stroke(Color(red: 1, green: 0.56, blue: 0.24), lineWidth: 1)
-              )
-              Spacer()
             }
-          HStack{ZStack() {
-              Rectangle()
-                  .foregroundColor(.clear)
-                  .frame(width: 85, height: 30)
-                  .background(Color(red: 1, green: 0.56, blue: 0.24))
-                  .cornerRadius(25)
-                  .offset(x: 0, y: 0)
-              Text(sensor.isStarted ? "終了" : "開始")
-                  .font(.system(size: 20))
-                  .foregroundColor(.white)
+            .frame(width: 70, height: 70)
+            .overlay(
+              RoundedRectangle(cornerRadius: 10)
+                .inset(by: 1)
+                .stroke(Color.accentColor, lineWidth: 1)
+            )
+            Spacer()
           }
-          //.frame(width: 125, height: 52)
-          }
-          HStack{
-              Image(systemName: "figure.walk")
-              Text("\(sensor.stepCount)")
-              Spacer()
-              Image(systemName: "clock")
-              Text("Time: \(sensor.elapsedTimeString)")
-          }
-          .overlay(
-            Divider()
-            .background(.white)
-            .offset(y: 10)
-          )
-          .frame(width: .infinity)
-          
-          HStack {
-              Image(systemName: "heart")
-              Text("\(sensor.heartRate) BPM")
-              Spacer()
-              Image(systemName: "flame")
-              Text("\(sensor.calories) kcal")
-          }
-          .foregroundColor(.accentColor)
-          .frame(width: .infinity)
-      }
-  }
-}
+    }
+    
+    private var startAndStop: some View {
+        Group {
+            if sensor.isStarted {
+                // 当已开始，显示这个按钮样式
+                Button(action: {
+                    sensor.stop()
+                }) {
+                    HStack{
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.accentColor)
+                                .frame(width: 85, height: 30)
+                                .cornerRadius(25)
+                            Text("終了")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        }.frame(width: 100, height: 45)
+                            .offset(y: 10)
+                    }
+                }
+            } else {
+                // 当未开始，显示这个按钮样式
+                Button(action: {
+                    sensor.start()
+                }) {
+                    ZStack {
+                        StartBottunBackGround()
+                        Text("開始")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                    }
+                    .frame(width: 100, height: 100)
+                }
+            }
+        }
+        .offset(x: 0, y: -10)
+        .buttonStyle(PlainButtonStyle())
+    }
 
+    
+    private var settingAndUpload: some View {
+        HStack{
+            Button(action: {
+                showTimePicker = true
+            }) {
+                Image(systemName: "gearshape.fill")
+                    .font(.system(size: 15))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Circle().frame(width: 28).foregroundColor(Color("DarkGray")))
+            }
+            .buttonStyle(PlainButtonStyle())
+            .sheet(isPresented: $showTimePicker) {
+                TimePickerView(showPicker: $showTimePicker, startDate: $startDate, endDate: $endDate)
+            }
+            
+            Text("\(sensor.elapsedTimeString)")
+            
+            Button(action: handleUploadAction) {
+                Image(systemName: "square.and.arrow.up.fill")
+                    .font(.system(size: 14))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Circle().frame(width: 28).foregroundColor(Color("DarkGray")))
+            }
+            .buttonStyle(PlainButtonStyle())
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("alart"), message: Text(alertMessage), dismissButton: .default(Text("确定")))
+            }
+
+
+        }
+    }
+    
+    private var stepsAndTiming: some View {
+        HStack{
+            Image(systemName: "figure.walk")
+            Text("\(sensor.stepCount)")
+            Spacer()
+            Image(systemName: "clock")
+            Text("\(sensor.elapsedTimeString)")
+        }
+        .padding(.vertical, 3)
+        /*
+        .overlay(
+          Divider()
+          .background(.white)
+          .offset(y: 14)
+        )
+        .frame(width: .infinity)*/
+    }
+    
+    private var heartRateAndCalories: some View {
+        HStack {
+            Image(systemName: "heart")
+            Text("\(sensor.heartRate) BPM")
+            Spacer()
+            Image(systemName: "flame")
+            Text("\(sensor.calories) kcal")
+        }
+        .padding(.vertical, 3)
+        .foregroundColor(.accentColor)
+        
+    }
+    
+    func handleUploadAction() {
+            sensor.uploadDataToAPI { (success, error) in
+                if success {
+                    showAlert = true
+                    alertMessage = "アップロード成功!"
+                } else if let error = error {
+                    showAlert = true
+                    alertMessage = "アップロード失敗: \(error.localizedDescription)"
+                }
+            }
+        }
+}
 
 struct StartBottunBackGround: View {
     var body: some View {
         ZStack {
             Circle().foregroundColor(Color("DarkRed")).frame(width: 105)
             Circle().foregroundColor(Color("MiddleRed")).frame(width: 85)
-            Circle()
-                .foregroundColor(.accentColor).frame(width: 70)
+            Circle().foregroundColor(.accentColor).frame(width: 70)
         }
     }
 }
 
+struct TimePickerView: View {
+    @Binding var showPicker: Bool
+    @Binding var startDate: Date
+    @Binding var endDate: Date
+    
+    var body: some View {
+        VStack() {
+            DatePicker("开始时间", selection: $startDate, displayedComponents: .hourAndMinute)
+            DatePicker("结束时间", selection: $endDate, displayedComponents: .hourAndMinute)
+            Button("完成") {
+                showPicker = false
+            }
+            .buttonStyle(PlainButtonStyle())
+            .frame(width: 100,height: 40)
+        }
+        .padding()
+    }
+}
+
+struct GradientDivider: View {
+    var body: some View {
+        Rectangle()
+            .fill(
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        .clear,
+                        .darkGray,
+                        .clear
+                    ]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            )
+            .frame(height: 1)
+    }
+}
+
+
 #Preview {
     MotionSensorScreen()
-    //TimingView()
 }
